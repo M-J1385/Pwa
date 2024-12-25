5fa0c0723e18c5599e6ba61cfcf4c34ab69478c9
M-J1385 <147722010+M-J1385@users.noreply.github.com>
2024-12-25 18:54:17 +0330

    Update app.js

--- js/app.js
+++ js/app.js
@@ -37,3 +37,21 @@ if ("serviceWorker" in navigator) {
   });
 }
 });
+document.addEventListener("DOMContentLoaded", function(){
+  const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "vertical",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+});
