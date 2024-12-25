3fa25c9fc20550719f0d41fe9c799caa5f757e72
M-J1385 <147722010+M-J1385@users.noreply.github.com>
2024-12-25 18:58:45 +0330

    Update app.js

--- js/app.js
+++ js/app.js
@@ -36,21 +36,4 @@ if ("serviceWorker" in navigator) {
       .catch((err) => console.log("service worker not registered", err));
   });
 }
-document.addEventListener("DOMContentLoaded", function(){
-  const swiper = new Swiper(".swiper", {
-  // Optional parameters
-  direction: "vertical",
-  loop: true,
-
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
 });
