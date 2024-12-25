88f0e7cb465e33b9183f8a58c878cabbc657fb88
M-J1385 <147722010+M-J1385@users.noreply.github.com>
2024-12-25 16:51:19 +0330

    Update app.js

--- js/app.js
+++ js/app.js
@@ -31,7 +31,7 @@ document.addEventListener("DOMContentLoaded", showCoffees);
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function () {
     navigator.serviceWorker
-      .register("https://github.com/M-J1385/Pwa/serviceWorker.js")
+      .register("/Pwa/serviceWorker.js")
       .then((res) => console.log("service worker registered"))
       .catch((err) => console.log("service worker not registered", err));
   });
